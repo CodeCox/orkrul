283bbc7017f76b245c15bc2298065617356d11fe
CodeCox <codecox@users.noreply.github.com>
2019-01-23 08:29:19 +0000

    init

--- testy/src/main/kotlin/Sketch.kt
+++ testy/src/main/kotlin/Sketch.kt
@@ -1,3 +1,12 @@
-fun main(){
-    println("testy")
+import org.openrndr.application
+
+fun main() = application {
+    configure {
+        title = "or"
+    }
+    program {
+        extend {
+            drawer.circle(width / 2.0, height / 2.0, 100.0)
+        }
+    }
 }
